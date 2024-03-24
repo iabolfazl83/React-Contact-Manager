0cd55c4c849229878f7397277f1f25acd6a99449
Abolfazl <iabolfazl83@yahoo.com>
2024-03-25 01:00:31 +0330

    refactored index.js and added routes.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Contacts from "./components/Contacts/Contacts";
+import AddContact from "./components/Contacts/AddContact";
+import EditContact from "./components/Contacts/EditContact";
+import ViewContact from "./components/Contacts/ViewContact";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}>
+                    <Route path="/contacts" element={<Contacts/>}/>
+                    <Route path="/contacts/add" element={<AddContact/>}/>
+                    <Route path="/contacts/:contactId" element={<ViewContact/>}/>
+                    <Route path="/contacts/edit/:contactId" element={<EditContact/>}/>
+                </Route>
+                <Route path="*" element={
+                    <main style={{width: "25%", margin: "0 auto"}}>
+                        <img style={{width: "100%"}} src={require("./assets/404.gif")} alt="404 error"/>
+                    </main>
+                }/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
