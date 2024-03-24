5ab2b45a551acdf259d34ee1450ac6fb1f12d058
Abolfazl <iabolfazl83@yahoo.com>
2024-03-25 01:03:53 +0330

    created SearchContact.js Component.

---
+++ src/components/Contacts/SearchContact.js
@@ -0,0 +1,31 @@
+import {PURPLE, COMMENT} from "../../helpers/colors";
+import {useContext} from "react";
+import {ContactContext} from "../../context/contactContext";
+
+const SearchContact = () => {
+    const {contactQuery, contactSearch} = useContext(ContactContext)
+
+    return (
+        <div className="input-group mx-2 w-75" dir="ltr">
+      <span
+          className="input-group-text"
+          id="basic-addon1"
+          style={{backgroundColor: PURPLE}}>
+        <i className="fas fa-search"/>
+      </span>
+            <input
+                dir="rtl"
+                type="text"
+                value={contactQuery.text}
+                onChange={contactSearch}
+                style={{backgroundColor: COMMENT, borderColor: PURPLE}}
+                className="form-control"
+                placeholder="جستجوی مخاطب"
+                aria-label="Search"
+                aria-describedby="basic-addon1"
+            />
+        </div>
+    );
+};
+
+export default SearchContact;
