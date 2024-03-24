4912f395e510a2bdbfea0844d33720885d1a80ec
Abolfazl <iabolfazl83@yahoo.com>
2024-03-25 00:36:45 +0330

    created contactContext.js Context.

---
+++ src/context/contactContext.js
@@ -0,0 +1,16 @@
+import {createContext} from "react";
+
+export const ContactContext = createContext({
+    loading: false, setLoading: () => {},
+    contact: {},
+    contacts: [],
+    setContacts: [],
+    filteredContacts: [],
+    setFilteredContacts: [],
+    contactQuery: {},
+    groups: [],
+    onContactChange: () => {},
+    deleteContact: () => {},
+    createContact: () => {},
+    contactSearch: () => {},
+});
